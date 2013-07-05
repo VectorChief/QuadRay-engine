1dcb74d413f00f80860adea6d46e32c3bf942201
VectorChief <vectorchiefrocks@gmail.com>
2013-07-05 18:50:20 +0300

    arch definition for x86 SSE

---
+++ core/rtarch_x86_sse.h
@@ -0,0 +1,42 @@
+/******************************************************************************/
+/* Copyright (c) 2013 VectorChief (at github, bitbucket, sourceforge)         */
+/* Distributed under the MIT software license, see the accompanying           */
+/* file COPYING or http://www.opensource.org/licenses/mit-license.php         */
+/******************************************************************************/
+
+#ifndef RT_RTARCH_X86_SSE_H
+#define RT_RTARCH_X86_SSE_H
+
+#include "rtarch_x86.h"
+
+/* registers    MOD,  REG,  SIB */
+
+#define Xmm0    0x03, 0x00, EMPTY
+#define Xmm1    0x03, 0x01, EMPTY
+#define Xmm2    0x03, 0x02, EMPTY
+#define Xmm3    0x03, 0x03, EMPTY
+#define Xmm4    0x03, 0x04, EMPTY
+#define Xmm5    0x03, 0x05, EMPTY
+#define Xmm6    0x03, 0x06, EMPTY
+#define Xmm7    0x03, 0x07, EMPTY
+
+/******************************************************************************/
+/**********************************   SSE   ***********************************/
+/******************************************************************************/
+
+/* fpu (SSE1) */
+
+#define movps_rr(RG, RM)                                                    \
+        EMITB(0x0F) EMITB(0x28) MRM(MOD(RM), REG(RG), REG(RM), SIB(RM))
+
+#define movps_ld(RG, RM, DP)                                                \
+        EMITB(0x0F) EMITB(0x28) MRM(MOD(RM), REG(RG), REG(RM), SIB(RM)) DP
+
+#define movps_st(RG, RM, DP)                                                \
+        EMITB(0x0F) EMITB(0x29) MRM(MOD(RM), REG(RG), REG(RM), SIB(RM)) DP
+
+#endif /* RT_RTARCH_X86_SSE_H */
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
