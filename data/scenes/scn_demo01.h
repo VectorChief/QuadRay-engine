b4df51d93ecb375a49690f6db6dbd4e79a1d4d77
VectorChief <vectorchiefrocks@gmail.com>
2013-07-29 21:10:20 +0400

    add custom clippers to scene data

--- data/scenes/scn_demo01.h
+++ data/scenes/scn_demo01.h
@@ -110,7 +110,7 @@ rt_OBJECT ob_base01[] =
 };
 
 /******************************************************************************/
-/********************************   BALLS   ***********************************/
+/*******************************   FIGURES   **********************************/
 /******************************************************************************/
 
 rt_SPHERE  sp_ball01 =
@@ -138,6 +138,31 @@ rt_SPHERE  sp_ball01 =
 /* rad */   1.5,
 };
 
+rt_PARABOLOID  pb_para01 =
+{
+    {      /*   RT_I,       RT_J,       RT_K    */
+/* min */   {  -RT_INF,    -RT_INF,    -RT_INF  },
+/* max */   {  +RT_INF,    +RT_INF,    +RT_INF  },
+        {
+/* OUTER        RT_U,       RT_V    */
+/* scl */   {    1.0,        1.0    },
+/* rot */              0.0           ,
+/* pos */   {    0.0,        0.0    },
+
+/* mat */   &mt_pink01,
+        },
+        {
+/* INNER        RT_U,       RT_V    */
+/* scl */   {    1.0,        1.0    },
+/* rot */              0.0           ,
+/* pos */   {    0.0,        0.0    },
+
+/* mat */   &mt_gray01,
+        },
+    },
+/* par */   1.0,
+};
+
 rt_SPHERE  sp_ball02 =
 {
     {      /*   RT_I,       RT_J,       RT_K    */
@@ -163,7 +188,7 @@ rt_SPHERE  sp_ball02 =
 /* rad */   0.5,
 };
 
-rt_OBJECT  ob_balls01[] =
+rt_OBJECT  ob_figures01[] =
 {
     {
         {  /*   RT_X,       RT_Y,       RT_Z    */
@@ -172,6 +197,22 @@ rt_OBJECT  ob_balls01[] =
 /* pos */   {    0.0,        0.0,        1.5    },
         },
             RT_OBJ_SPHERE(&sp_ball01)
+    },
+    {
+        {  /*   RT_X,       RT_Y,       RT_Z    */
+/* scl */   {    1.0,        1.0,        1.0    },
+/* rot */   {    0.0,        0.0,        0.0    },
+/* pos */   {    0.0,        0.0,        2.0    },
+        },
+            RT_OBJ_PARABOLOID(&pb_para01)
+    },
+    {
+        {  /*   RT_X,       RT_Y,       RT_Z    */
+/* scl */   {    1.0,        1.0,        1.0    },
+/* rot */   {    0.0,       90.0,        0.0    },
+/* pos */   {   -1.5,       -1.5,        1.5    },
+        },
+            RT_OBJ_CYLINDER(&cl_tube01)
     },
     {
         {  /*   RT_X,       RT_Y,       RT_Z    */
@@ -205,6 +246,14 @@ rt_OBJECT  ob_balls01[] =
         },
             RT_OBJ_SPHERE(&sp_ball02)
     },
+};
+
+rt_RELATION  rl_figures01[] =
+{
+    {   0,  RT_REL_MINUS_INNER,   1   },
+    {   1,  RT_REL_MINUS_OUTER,   0   },
+    {   0,  RT_REL_MINUS_INNER,   2   },
+    {   2,  RT_REL_MINUS_OUTER,   0   },
 };
 
 /******************************************************************************/
@@ -243,7 +292,7 @@ rt_OBJECT ob_tree[] =
 /* rot */   {    0.0,        0.0,        0.0    },
 /* pos */   {    0.0,        0.0,        0.0    },
         },
-            RT_OBJ_ARRAY(&ob_balls01),
+            RT_OBJ_ARRAY_REL(&ob_figures01, &rl_figures01),
     },
     {
         {  /*   RT_X,       RT_Y,       RT_Z    */
