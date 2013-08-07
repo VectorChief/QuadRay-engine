af17266e388ed63055f1d43f4dbf2d810e8520eb
VectorChief <vectorchiefrocks@gmail.com>
2013-08-07 21:10:54 +0400

    add more custom clipping to scene data

--- data/scenes/scn_demo01.h
+++ data/scenes/scn_demo01.h
@@ -158,8 +158,8 @@ rt_OBJECT ob_column02[] =
     {
         {  /*   RT_X,       RT_Y,       RT_Z    */
 /* scl */   {    1.0,        0.2,        0.2    },
-/* rot */   {    0.0,        0.0,        0.0    },
-/* pos */   {    0.0,        0.0,        1.0    },
+/* rot */   {    0.0,       20.0,        0.0    },
+/* pos */   {    0.0,        0.0,        1.5    },
         },
         RT_OBJ_ARRAY(&ob_cube01)
     },
@@ -185,7 +185,7 @@ rt_OBJECT ob_base01[] =
     {
         {  /*   RT_X,       RT_Y,       RT_Z    */
 /* scl */   {    1.0,        1.0,        1.0    },
-/* rot */   {    0.0,        0.0,        0.0    },
+/* rot */   {    0.0,        0.0,     -135.0    },
 /* pos */   {   -4.5,       -4.5,        0.0    },
         },
         RT_OBJ_ARRAY_REL(&ob_column02, &rl_column02)
@@ -193,7 +193,7 @@ rt_OBJECT ob_base01[] =
     {
         {  /*   RT_X,       RT_Y,       RT_Z    */
 /* scl */   {    1.0,        1.0,        1.0    },
-/* rot */   {    0.0,        0.0,        0.0    },
+/* rot */   {    0.0,        0.0,      -45.0    },
 /* pos */   {   +4.5,       -4.5,        0.0    },
         },
         RT_OBJ_ARRAY_REL(&ob_column02, &rl_column02)
@@ -256,7 +256,7 @@ rt_SPHERE sp_ball01 =
 {
     {      /*   RT_I,       RT_J,       RT_K    */
 /* min */   {  -RT_INF,    -RT_INF,    -RT_INF  },
-/* max */   {  +RT_INF,    +RT_INF,    +RT_INF  },
+/* max */   {  +RT_INF,    +RT_INF,     +1.3    },
         {
 /* OUTER        RT_U,       RT_V    */
 /* scl */   {    1.0,        1.0    },
@@ -288,7 +288,7 @@ rt_PARABOLOID pb_para01 =
 /* rot */              0.0           ,
 /* pos */   {    0.0,        0.0    },
 
-/* mat */   &mt_pink01,
+/* mat */   &mt_gray01,
         },
         {
 /* INNER        RT_U,       RT_V    */
@@ -299,7 +299,32 @@ rt_PARABOLOID pb_para01 =
 /* mat */   &mt_gray01,
         },
     },
-/* par */   1.0,
+/* par */   0.3,
+};
+
+rt_CYLINDER cl_tube02 =
+{
+    {      /*   RT_I,       RT_J,       RT_K    */
+/* min */   {  -RT_INF,    -RT_INF,      0.0     },
+/* max */   {  +RT_INF,    +RT_INF,     +1.5     },
+        {
+/* OUTER        RT_U,       RT_V    */
+/* scl */   {    1.0,        1.0    },
+/* rot */              0.0           ,
+/* pos */   {    0.0,        0.0    },
+
+/* mat */   &mt_gray01,
+        },
+        {
+/* INNER        RT_U,       RT_V    */
+/* scl */   {    1.0,        1.0    },
+/* rot */              0.0           ,
+/* pos */   {    0.0,        0.0    },
+
+/* mat */   &mt_gray01,
+        },
+    },
+/* rad */   3.0,
 };
 
 rt_SPHERE sp_ball02 =
@@ -352,7 +377,7 @@ rt_CONE cn_cone01 =
 /* rat */   1.0,
 };
 
-rt_OBJECT ob_figures01[] =
+rt_OBJECT ob_ball01[] =
 {
     {
         {  /*   RT_X,       RT_Y,       RT_Z    */
@@ -366,17 +391,48 @@ rt_OBJECT ob_figures01[] =
         {  /*   RT_X,       RT_Y,       RT_Z    */
 /* scl */   {    1.0,        1.0,        1.0    },
 /* rot */   {    0.0,        0.0,        0.0    },
-/* pos */   {    0.0,        0.0,        2.0    },
+/* pos */   {   +1.0,        0.0,        2.5    },
+        },
+        RT_OBJ_SPHERE_MAT(&sp_ball02, &mt_gray01, &mt_gray01)
+    },
+    {
+        {  /*   RT_X,       RT_Y,       RT_Z    */
+/* scl */   {    1.0,        1.0,        1.0    },
+/* rot */   {    0.0,       90.0,        0.0    },
+/* pos */   {    0.0,        0.0,        5.5    },
+        },
+        RT_OBJ_CYLINDER(&cl_tube02)
+    },
+};
+
+rt_RELATION rl_ball01[] =
+{
+    {  -1,  RT_REL_MINUS_OUTER,   1   },
+    {  -1,  RT_REL_MINUS_INNER,   2   },
+    {   0,  RT_REL_MINUS_ACCUM,  -1   },
+    {   1,  RT_REL_MINUS_OUTER,   0   },
+    {   2,  RT_REL_MINUS_OUTER,   0   },
+    {   1,  RT_REL_MINUS_INNER,   2   },
+    {   2,  RT_REL_MINUS_OUTER,   1   },
+};
+
+rt_OBJECT ob_figures01[] =
+{
+    {
+        {  /*   RT_X,       RT_Y,       RT_Z    */
+/* scl */   {    0.5,        1.0,        1.0    },
+/* rot */   {    0.0,      -60.0,        0.0    },
+/* pos */   {   -0.5,        0.0,        2.0    },
         },
         RT_OBJ_PARABOLOID(&pb_para01)
     },
     {
         {  /*   RT_X,       RT_Y,       RT_Z    */
 /* scl */   {    1.0,        1.0,        1.0    },
-/* rot */   {    0.0,       90.0,        0.0    },
-/* pos */   {   -1.5,       -1.5,        1.5    },
+/* rot */   {    0.0,        0.0,        0.0    },
+/* pos */   {    0.0,        0.0,        0.0    },
         },
-        RT_OBJ_CYLINDER(&cl_tube01)
+        RT_OBJ_ARRAY_REL(&ob_ball01, &rl_ball01),
     },
     {
         {  /*   RT_X,       RT_Y,       RT_Z    */
@@ -430,10 +486,10 @@ rt_OBJECT ob_figures01[] =
 
 rt_RELATION rl_figures01[] =
 {
-    {   0,  RT_REL_MINUS_INNER,   1   },
-    {   1,  RT_REL_MINUS_OUTER,   0   },
-    {   0,  RT_REL_MINUS_INNER,   2   },
-    {   2,  RT_REL_MINUS_OUTER,   0   },
+    {   1,  RT_REL_INDEX_ARRAY,  -1   },
+    {   0,  RT_REL_MINUS_INNER,   0   },
+    {  -1,  RT_REL_INDEX_ARRAY,   1   },
+    {   0,  RT_REL_MINUS_OUTER,   0   },
 };
 
 /******************************************************************************/
