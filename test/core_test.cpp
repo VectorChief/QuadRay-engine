c5b37d5ac27f0ad84aa581b2a4af5250a2697061
VectorChief <vectorchiefrocks@gmail.com>
2014-01-18 16:00:33 +0200

    core test framework, run level 7

--- test/core_test.cpp
+++ test/core_test.cpp
@@ -9,7 +9,7 @@
 
 #include "engine.h"
 
-#define RUN_LEVEL       6
+#define RUN_LEVEL       7
 #define VERBOSE         RT_FALSE
 #define CYC_SIZE        10
 
@@ -167,6 +167,24 @@ rt_void test06(rt_cell opts)
 #endif /* RUN_LEVEL  6 */
 
 
+#if RUN_LEVEL >=  7
+
+#include "scn_test07.h"
+
+rt_void test07(rt_cell opts)
+{
+    scene = new rt_Scene(&scn_test07::sc_root,
+                        x_res, y_res, x_row, RT_NULL,
+                        malloc, free,
+                        RT_NULL, RT_NULL,
+                        RT_NULL, RT_NULL);
+
+    scene->set_opts(opts);
+}
+
+#endif /* RUN_LEVEL  7 */
+
+
 typedef rt_void (*testXX)(rt_cell);
 
 testXX test[RUN_LEVEL] =
@@ -194,6 +212,10 @@ testXX test[RUN_LEVEL] =
 #if RUN_LEVEL >=  6
     test06,
 #endif /* RUN_LEVEL  6 */
+
+#if RUN_LEVEL >=  7
+    test07,
+#endif /* RUN_LEVEL  7 */
 };
 
 
