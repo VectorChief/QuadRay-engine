64dd3c20c21d70b1e15cfc3aaa0b7d7c05138d4f
VectorChief <vectorchiefrocks@gmail.com>
2014-06-22 16:08:22 +0300

    swap mtx_has_trm and obj_has_trm, fix assignment

--- core/engine/object.h
+++ core/engine/object.h
@@ -172,12 +172,13 @@ class rt_Object
     rt_real            *pos;
     rt_cell             tag;
 
-    /* non-zero if object itself has
+    /* non-zero if object itself or
+     * some of its parents has
      * non-trivial transform
      * (scaling, rotation or both) */
     rt_cell             obj_has_trm;
 
-    /* non-zero if object's full matrix has
+    /* non-zero if object's own matrix has
      * non-trivial transform
      * (scaling, rotation or both) */
     rt_cell             mtx_has_trm;
