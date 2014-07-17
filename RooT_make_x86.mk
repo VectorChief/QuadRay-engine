2964b18ff3333510e55ef719b57e12e2749d3315
VectorChief <vectorchiefrocks@gmail.com>
2014-07-17 21:57:49 +0400

    fix compilation on 64-bit Linux systems

--- RooT_make_x86.mk
+++ RooT_make_x86.mk
@@ -27,7 +27,7 @@ LIB_LIST =                          \
         -lpthread
 
 RooT:
-	g++ -O3 -g -DRT_PATH="./" \
+	g++ -O3 -g -m32 -DRT_PATH="./" \
         -DRT_LINUX -DRT_X86 -DRT_DEBUG=0 -DRT_FULLSCREEN=0 \
         -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o RooT.x86
--- test/core_make_x86.mk
+++ test/core_make_x86.mk
@@ -18,7 +18,7 @@ SRC_LIST =                          \
         core_test.cpp
 
 core_test:
-	g++ -O3 -g -DRT_PATH="../" \
+	g++ -O3 -g -m32 -DRT_PATH="../" \
         -DRT_LINUX -DRT_X86 -DRT_DEBUG=1 \
         -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
         ${INC_PATH} ${SRC_LIST} -o core_test.x86
