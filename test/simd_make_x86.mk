26fa5e39075c94ad26d6ff9591e6b934d4313997
VectorChief <vectorchiefrocks@gmail.com>
2014-07-17 21:10:40 +0400

    fix compilation on 64-bit Linux systems

--- test/simd_make_x86.mk
+++ test/simd_make_x86.mk
@@ -1,9 +1,9 @@
 INC_PATH =                          \
-        -I../core/config
+        -I../core/config/
 
 SRC_LIST =                          \
         simd_test.cpp
 
 simd_test:
-	g++ -O3 -g -DRT_LINUX -DRT_X86 -DRT_DEBUG=0 \
+	g++ -O3 -g -m32 -DRT_LINUX -DRT_X86 -DRT_DEBUG=0 \
         ${INC_PATH} ${SRC_LIST} -o simd_test.x86
