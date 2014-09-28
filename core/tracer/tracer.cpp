7b6282549cc1637153f6b07d250977e5da16ba66
VectorChief <vectorchiefrocks@gmail.com>
2014-09-28 18:26:39 +0300

    optimize overdraw for paraboloid

--- core/tracer/tracer.cpp
+++ core/tracer/tracer.cpp
@@ -15,6 +15,7 @@
 /* Conditional compilation flags
  * for respective segments of code.
  */
+#define RT_ANTIALIASING         1
 #define RT_CLIPPING_MINMAX      1
 #define RT_CLIPPING_CUSTOM      1
 #define RT_TEXTURING            1
@@ -26,7 +27,6 @@
 #define RT_REFLECTIONS          1
 #define RT_TRANSPARENCY         1
 #define RT_REFRACTIONS          1
-#define RT_ANTIALIASING         1
 
 /* Byte-offsets within SIMD-field
  * for packed scalar fields.
@@ -2084,6 +2084,10 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
         movxx_mi(Mecx, ctx_LOCAL(FLG), IB(FLAG_SIDE_OUTER))
         SUBROUTINE(CN_mt1, CN_mat)
 
+        /* optimize overdraw */
+        /* not applicable as inner and outer roots swap places
+         * along the ray's direction based on "a_val's" sign */
+
 /******************************************************************************/
     LBL(CN_rt2)
 
@@ -2286,6 +2290,10 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
         /* material */
         movxx_mi(Mecx, ctx_LOCAL(FLG), IB(FLAG_SIDE_OUTER))
         SUBROUTINE(PB_mt1, PB_mat)
+
+        /* optimize overdraw */
+        movpx_ld(Xmm7, Mecx, ctx_TMASK(0))      /* tmask <- TMASK */
+        CHECK_MASK(OO_end, FULL, Xmm7)
 
 /******************************************************************************/
     LBL(PB_rt2)
@@ -2496,6 +2504,10 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
         movxx_mi(Mecx, ctx_LOCAL(FLG), IB(FLAG_SIDE_OUTER))
         SUBROUTINE(HB_mt1, HB_mat)
 
+        /* optimize overdraw */
+        /* not applicable as inner and outer roots swap places
+         * along the ray's direction based on "a_val's" sign */
+
 /******************************************************************************/
     LBL(HB_rt2)
 
@@ -2510,7 +2522,6 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
 
         /* clipping */
         movpx_ld(Xmm7, Mecx, ctx_XMASK)         /* xmask <- XMASK */
-
         SUBROUTINE(HB_cp2, CC_clp)
         CHECK_MASK(OO_end, NONE, Xmm7)
         movpx_st(Xmm7, Mecx, ctx_TMASK(0))      /* tmask -> TMASK */
