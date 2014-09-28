9fa2fb22e9f67e694517e59d237114d330e5b720
VectorChief <vectorchiefrocks@gmail.com>
2014-09-28 14:40:46 +0300

    optimize overdraw for paraboloid

--- core/tracer/tracer.cpp
+++ core/tracer/tracer.cpp
@@ -19,6 +19,7 @@
 #define RT_SHOW_TILES               0
 
 #define RT_FEAT_TILING              1
+#define RT_FEAT_ANTIALIASING        1
 #define RT_FEAT_MULTITHREADING      1
 #define RT_FEAT_CLIPPING_MINMAX     1
 #define RT_FEAT_CLIPPING_CUSTOM     1
@@ -32,7 +33,6 @@
 #define RT_FEAT_REFLECTIONS         1
 #define RT_FEAT_TRANSPARENCY        1
 #define RT_FEAT_REFRACTIONS         1
-#define RT_FEAT_ANTIALIASING        1
 #define RT_FEAT_TRANSFORM           1
 #define RT_FEAT_TRANSFORM_ARRAY     1
 #define RT_FEAT_BOUND_VOL_ARRAY     1
@@ -2754,6 +2754,10 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
         movxx_mi(Mecx, ctx_LOCAL(FLG), IB(RT_FLAG_SIDE_OUTER))
         SUBROUTINE(CN_mt1, CN_mat)
 
+        /* optimize overdraw */
+        /* not applicable as inner and outer roots swap places
+         * along the ray's direction based on "a_val's" sign */
+
 /******************************************************************************/
     LBL(CN_rt2)
 
@@ -2964,6 +2968,10 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
         /* material */
         movxx_mi(Mecx, ctx_LOCAL(FLG), IB(RT_FLAG_SIDE_OUTER))
         SUBROUTINE(PB_mt1, PB_mat)
+
+        /* optimize overdraw */
+        movpx_ld(Xmm7, Mecx, ctx_TMASK(0))      /* tmask <- TMASK */
+        CHECK_MASK(OO_end, FULL, Xmm7)
 
 /******************************************************************************/
     LBL(PB_rt2)
@@ -3182,6 +3190,10 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
         movxx_mi(Mecx, ctx_LOCAL(FLG), IB(RT_FLAG_SIDE_OUTER))
         SUBROUTINE(HB_mt1, HB_mat)
 
+        /* optimize overdraw */
+        /* not applicable as inner and outer roots swap places
+         * along the ray's direction based on "a_val's" sign */
+
 /******************************************************************************/
     LBL(HB_rt2)
 
@@ -3196,7 +3208,6 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
 
         /* clipping */
         movpx_ld(Xmm7, Mecx, ctx_XMASK)         /* xmask <- XMASK */
-
         SUBROUTINE(HB_cp2, CC_clp)
         CHECK_MASK(OO_end, NONE, Xmm7)
         movpx_st(Xmm7, Mecx, ctx_TMASK(0))      /* tmask -> TMASK */
