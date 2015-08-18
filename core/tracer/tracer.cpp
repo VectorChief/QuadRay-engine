28a517e7cba426eb67a437e1f332563199188125
VectorChief <vectorchiefrocks@gmail.com>
2015-08-18 15:56:14 +0400

    fix typos in rendering backend

--- core/tracer/tracer.cpp
+++ core/tracer/tracer.cpp
@@ -95,9 +95,9 @@
  * transform is a multiple of 90 degree rotation.
  */
 #define INDEX_AXIS(nx)                                                      \
-        movxx_ld(Reax, Mebx, srf_A_SGN(nx * 4))                             \
+        movxx_ld(Reax, Mebx, srf_A_SGN(nx*4))                               \
         movpx_ld(Xmm0, Iebx, srf_SBASE)                                     \
-        movxx_ld(Reax, Mebx, srf_A_MAP(nx * 4))
+        movxx_ld(Reax, Mebx, srf_A_MAP(nx*4))
 
 #define MOVXR_LD(RG, RM, DP)                                                \
         movpx_ld(W(RG), W(RM), W(DP))                                       \
@@ -112,14 +112,14 @@
         movpx_st(W(RG), W(RM), W(DP))
 
 #define INDEX_TMAP(nx)                                                      \
-        movxx_ld(Reax, Medx, mat_T_MAP(nx * 4))
+        movxx_ld(Reax, Medx, mat_T_MAP(nx*4))
 
 /* Axis clipping.
  * Check if axis clipping (minmax) is needed for given axis "nx",
  * jump to "lb" otherwise.
  */
 #define CHECK_CLIP(lb, pl, nx)                                              \
-        cmpxx_mi(Mebx, srf_##pl(nx * 4), IB(0))                             \
+        cmpxx_mi(Mebx, srf_##pl(nx*4), IB(0))                               \
         jeqxx_lb(lb)
 
 /* Custom clipping.
@@ -208,7 +208,6 @@
  */
 #define FETCH_PROP()                                                        \
         movxx_ld(Reax, Mecx, ctx_LOCAL(FLG))                                \
-        andxx_ri(Reax, IB(FLAG_SIDE))                                       \
         shlxx_ri(Reax, IB(4))                                               \
         movpx_ld(Xmm7, Iebx, srf_SBASE)                                     \
         shrxx_ri(Reax, IB(1))                                               \
@@ -389,7 +388,7 @@ rt_void update_mat(rt_SIMD_MATERIAL *s_mat)
 }
 
 /* Backend's global entry point (hence 0).
- * Update surfaces's backend-specific fields.
+ * Update surface's backend-specific fields.
  */
 rt_void update0(rt_SIMD_SURFACE *s_srf)
 {
@@ -415,7 +414,7 @@ rt_void update0(rt_SIMD_SURFACE *s_srf)
 /******************************************************************************/
 
 /* Backend's global entry point (hence 0).
- * Render the frame based on the data structures
+ * Render frame based on the data structures
  * prepared by the engine.
  */
 rt_void render0(rt_SIMD_INFOX *s_inf)
@@ -520,7 +519,7 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
 
 #if RT_TRANSFORM
 
-        cmpxx_mi(Mebx, srf_A_MAP(RT_W * 4), IB(0))
+        cmpxx_mi(Mebx, srf_A_MAP(RT_L*4), IB(0))
         jeqxx_lb(OO_rot)
 
         /* transform diff */
@@ -647,7 +646,7 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
 
 #if RT_TRANSFORM
 
-        cmpxx_mi(Mebx, srf_A_MAP(RT_W * 4), IB(0))
+        cmpxx_mi(Mebx, srf_A_MAP(RT_L*4), IB(0))
         jeqxx_lb(CX_rot)
 
         movpx_ld(Xmm4, Mecx, ctx_RAY_I)         /* ray_i <- RAY_I */
@@ -693,7 +692,7 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
 
 #if RT_TRANSFORM
 
-        cmpxx_mi(Mebx, srf_A_MAP(RT_W * 4), IB(0))
+        cmpxx_mi(Mebx, srf_A_MAP(RT_L*4), IB(0))
         jeqxx_lb(CY_rot)
 
         movpx_ld(Xmm5, Mecx, ctx_RAY_J)         /* ray_j <- RAY_J */
@@ -739,7 +738,7 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
 
 #if RT_TRANSFORM
 
-        cmpxx_mi(Mebx, srf_A_MAP(RT_W * 4), IB(0))
+        cmpxx_mi(Mebx, srf_A_MAP(RT_L*4), IB(0))
         jeqxx_lb(CZ_rot)
 
         movpx_ld(Xmm6, Mecx, ctx_RAY_K)         /* ray_k <- RAY_K */
@@ -823,7 +822,7 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
 
 #if RT_TRANSFORM
 
-        cmpxx_mi(Mebx, srf_A_MAP(RT_W * 4), IB(0))
+        cmpxx_mi(Mebx, srf_A_MAP(RT_L*4), IB(0))
         jeqxx_lb(CC_rot)
 
         /* transform clip */
@@ -893,7 +892,7 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
 
 #if RT_TRANSFORM
 
-        cmpxx_mi(Mebx, srf_A_MAP(RT_W * 4), IB(0))
+        cmpxx_mi(Mebx, srf_A_MAP(RT_L*4), IB(0))
         jeqxx_lb(MT_nrm)
 
         /* transform normal */
