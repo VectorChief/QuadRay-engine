2e345a59a0a77294e888c70b1fb1645370d68cf0
VectorChief <vectorchiefrocks@gmail.com>
2015-11-17 19:18:30 +0200

    remap SIMD registers for ARM to avoid collision with div VFP fallback

--- core/config/rtarch_x86.h
+++ core/config/rtarch_x86.h
@@ -397,9 +397,9 @@
 /* div */
 
 #define divxn_xm(RM, DP) /* Reax is in/out, Redx is Reax-sign-extended */   \
-        EMITB(0xF7)      /* destroys Xmm0 (in ARM) */                       \
+        EMITB(0xF7)                                                         \
         MRM(0x07,    MOD(RM), REG(RM)) /* limited precision */              \
-        AUX(SIB(RM), CMD(DP), EMPTY)   /* fp div (in ARM) */
+        AUX(SIB(RM), CMD(DP), EMPTY)   /* fp div (in ARM, but not A32) */
 
 /* cmp */
 
