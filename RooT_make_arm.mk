0d41ef18e5001dcf05a3f44fe8f6b5de6525c317
VectorChief <vectorchiefrocks@gmail.com>
2016-02-11 15:35:19 +0000

    treat default ARM target as vanilla, add Nokia N900, Raspberry Pi 2

--- RooT_make_arm.mk
+++ RooT_make_arm.mk
@@ -29,8 +29,13 @@ LIB_LIST =                              \
         -lpthread
 
 RooT:
-	g++ -O3 -g -fexceptions \
+	g++ -O3 -g \
         -DRT_LINUX -DRT_ARM -DRT_128=1 \
-        -DRT_DEBUG=0 -DRT_PATH="./" -DRT_FULLSCREEN=1 \
-        -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=1 -DRT_EMBED_TEX=1 \
+        -DRT_DEBUG=0 -DRT_PATH="./" -DRT_FULLSCREEN=0 \
+        -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o RooT.arm
+
+# 0) The build flags above are intended for the default "vanilla" ARMv7 target,
+# while settings suitable for a specific hardware platform are presented below.
+# 1) Nokia N900, Maemo 5 scratchbox: -DRT_FULLSCREEN=1 -DRT_EMBED_FILEIO=1
+# 2) Raspberry Pi 2, Raspbian: -DRT_ARM=2
