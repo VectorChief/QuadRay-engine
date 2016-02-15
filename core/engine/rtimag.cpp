54c75cc8b5148f59cf75574a1f803a635f269e12
VectorChief <vectorchiefrocks@gmail.com>
2016-02-15 19:24:13 +0200

    implement endian-agnostic serialization for BMP format

--- core/engine/rtimag.cpp
+++ core/engine/rtimag.cpp
@@ -30,6 +30,18 @@
 /********************************   TEXTURE   *********************************/
 /******************************************************************************/
 
+/* endian-agnostic serialization from little-endian BMP format */
+
+#define RT_LOAD_H(h, p)                                                     \
+        h = (((rt_byte *)p)[0] << 0x00) |                                   \
+            (((rt_byte *)p)[1] << 0x08)
+
+#define RT_LOAD_W(w, p)                                                     \
+        w = (((rt_byte *)p)[0] << 0x00) |                                   \
+            (((rt_byte *)p)[1] << 0x08) |                                   \
+            (((rt_byte *)p)[2] << 0x10) |                                   \
+            (((rt_byte *)p)[3] << 0x18)
+
 /*
  * Load image from file to memory.
  */
@@ -37,7 +49,7 @@ rt_void load_image(rt_Heap *hp, rt_pstr name, rt_TEX *tx)
 {
 #if RT_EMBED_FILEIO == 0
     rt_word *p = RT_NULL;
-    rt_cell i, n;
+    rt_cell i, n, buf = 0;
 
     rt_pstr path = RT_PATH_TEXTURES;
     rt_cell len = strlen(path);
@@ -67,42 +79,59 @@ rt_void load_image(rt_Heap *hp, rt_pstr name, rt_TEX *tx)
         {
             break;
         }
-        if (f->load(&bsig, sizeof(bsig), 1) != 1)
+
+        if (f->load(&buf, sizeof(bsig), 1) != 1)
         {
             break;
         }
+        RT_LOAD_H(bsig, &buf);
+
         if (f->seek(10, SEEK_SET) != 0)
         {
             break;
         }
-        if (f->load(&boffset, sizeof(boffset), 1) != 1)
+
+        if (f->load(&buf, sizeof(boffset), 1) != 1)
         {
             break;
         }
+        RT_LOAD_W(boffset, &buf);
+
         if (f->seek(18, SEEK_SET) != 0)
         {
             break;
         }
-        if (f->load(&bwidth, sizeof(bwidth), 1) != 1)
+
+        if (f->load(&buf, sizeof(bwidth), 1) != 1)
         {
             break;
         }
-        if (f->load(&bheight, sizeof(bheight), 1) != 1)
+        RT_LOAD_W(bwidth, &buf);
+
+        if (f->load(&buf, sizeof(bheight), 1) != 1)
         {
             break;
         }
-        if (f->load(&bplanes, sizeof(bplanes), 1) != 1)
+        RT_LOAD_W(bheight, &buf);
+
+        if (f->load(&buf, sizeof(bplanes), 1) != 1)
         {
             break;
         }
-        if (f->load(&bdepth, sizeof(bdepth), 1) != 1)
+        RT_LOAD_H(bplanes, &buf);
+
+        if (f->load(&buf, sizeof(bdepth), 1) != 1)
         {
             break;
         }
-        if (f->load(&bzero, sizeof(bzero), 1) != 1)
+        RT_LOAD_H(bdepth, &buf);
+
+        if (f->load(&buf, sizeof(bzero), 1) != 1)
         {
             break;
         }
+        RT_LOAD_W(bzero, &buf);
+
         if (bsig != 0x4D42 || bplanes != 1 || bdepth != 24 || bzero != 0)
         {
             break;
@@ -150,6 +179,18 @@ rt_void load_image(rt_Heap *hp, rt_pstr name, rt_TEX *tx)
 #endif /* RT_EMBED_FILEIO */
 }
 
+/* endian-agnostic serialization to little-endian BMP format */
+
+#define RT_SAVE_H(h, p)                                                     \
+        ((rt_byte *)p)[0] = (h >> 0x00) & 0xFF;                             \
+        ((rt_byte *)p)[1] = (h >> 0x08) & 0xFF
+
+#define RT_SAVE_W(w, p)                                                     \
+        ((rt_byte *)p)[0] = (w >> 0x00) & 0xFF;                             \
+        ((rt_byte *)p)[1] = (w >> 0x08) & 0xFF;                             \
+        ((rt_byte *)p)[2] = (w >> 0x10) & 0xFF;                             \
+        ((rt_byte *)p)[3] = (w >> 0x18) & 0xFF
+
 /*
  * Save image from memory to file.
  */
@@ -157,7 +198,7 @@ rt_void save_image(rt_Heap *hp, rt_pstr name, rt_TEX *tx)
 {
 #if RT_EMBED_FILEIO == 0
     rt_word *p = RT_NULL;
-    rt_cell i, n;
+    rt_cell i, n, buf = 0;
 
     rt_pstr path = RT_PATH_DUMP;
     rt_cell len = strlen(path);
@@ -184,66 +225,95 @@ rt_void save_image(rt_Heap *hp, rt_pstr name, rt_TEX *tx)
         {
             break;
         }
-        if (f->save(&bsig, sizeof(bsig), 1) != 1)
+
+        RT_SAVE_H(bsig, &buf);
+        if (f->save(&buf, sizeof(bsig), 1) != 1)
         {
             break;
         }
-        if (f->save(&bsize, sizeof(bsize), 1) != 1)
+
+        RT_SAVE_W(bsize, &buf);
+        if (f->save(&buf, sizeof(bsize), 1) != 1)
         {
             break;
         }
-        if (f->save(&bzero, sizeof(bzero), 1) != 1)
+
+        RT_SAVE_W(bzero, &buf);
+        if (f->save(&buf, sizeof(bzero), 1) != 1)
         {
             break;
         }
-        if (f->save(&boffset, sizeof(boffset), 1) != 1)
+
+        RT_SAVE_W(boffset, &buf);
+        if (f->save(&buf, sizeof(boffset), 1) != 1)
         {
             break;
         }
-        if (f->save(&binfo, sizeof(binfo), 1) != 1)
+
+        RT_SAVE_W(binfo, &buf);
+        if (f->save(&buf, sizeof(binfo), 1) != 1)
         {
             break;
         }
-        if (f->save(&tx->x_dim, sizeof(tx->x_dim), 1) != 1)
+
+        RT_SAVE_W(tx->x_dim, &buf);
+        if (f->save(&buf, sizeof(tx->x_dim), 1) != 1)
         {
             break;
         }
-        if (f->save(&tx->y_dim, sizeof(tx->y_dim), 1) != 1)
+
+        RT_SAVE_W(tx->y_dim, &buf);
+        if (f->save(&buf, sizeof(tx->y_dim), 1) != 1)
         {
             break;
         }
-        if (f->save(&bplanes, sizeof(bplanes), 1) != 1)
+
+        RT_SAVE_H(bplanes, &buf);
+        if (f->save(&buf, sizeof(bplanes), 1) != 1)
         {
             break;
         }
-        if (f->save(&bdepth, sizeof(bdepth), 1) != 1)
+
+        RT_SAVE_H(bdepth, &buf);
+        if (f->save(&buf, sizeof(bdepth), 1) != 1)
         {
             break;
         }
-        if (f->save(&bzero, sizeof(bzero), 1) != 1)
+
+        RT_SAVE_W(bzero, &buf);
+        if (f->save(&buf, sizeof(bzero), 1) != 1)
         {
             break;
         }
 
         bsize -= boffset;
 
-        if (f->save(&bsize, sizeof(bsize), 1) != 1)
+        RT_SAVE_W(bsize, &buf);
+        if (f->save(&buf, sizeof(bsize), 1) != 1)
         {
             break;
         }
-        if (f->save(&bmeter, sizeof(bmeter), 1) != 1)
+
+        RT_SAVE_W(bmeter, &buf);
+        if (f->save(&buf, sizeof(bmeter), 1) != 1)
         {
             break;
         }
-        if (f->save(&bmeter, sizeof(bmeter), 1) != 1)
+
+        RT_SAVE_W(bmeter, &buf);
+        if (f->save(&buf, sizeof(bmeter), 1) != 1)
         {
             break;
         }
-        if (f->save(&bzero, sizeof(bzero), 1) != 1)
+
+        RT_SAVE_W(bzero, &buf);
+        if (f->save(&buf, sizeof(bzero), 1) != 1)
         {
             break;
         }
-        if (f->save(&bzero, sizeof(bzero), 1) != 1)
+
+        RT_SAVE_W(bzero, &buf);
+        if (f->save(&buf, sizeof(bzero), 1) != 1)
         {
             break;
         }
