e25fc44c0de32a1b161f16bc8ddceb46ff74cb0d
VectorChief <vectorchiefrocks@gmail.com>
2016-02-16 13:08:14 +0200

    turn off fullscreen mode by default across all targets

--- RooT_make_m32.mk
+++ RooT_make_m32.mk
@@ -31,6 +31,6 @@ LIB_LIST =                              \
 RooT:
 	mipsel-linux-gnu-g++ -O3 -g \
         -DRT_LINUX -DRT_M32 -DRT_128=1 \
-        -DRT_DEBUG=0 -DRT_PATH="./" -DRT_FULLSCREEN=1 \
+        -DRT_DEBUG=0 -DRT_PATH="./" -DRT_FULLSCREEN=0 \
         -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o RooT.m32
