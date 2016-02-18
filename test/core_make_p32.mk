8e42056128fb63b7e8221956df0aabd6ea33ce54
VectorChief <vectorchiefrocks@gmail.com>
2016-02-18 14:30:26 +0200

    remove custom build for p32 target, engine works with minor caveats

--- test/core_make_p32.mk
+++ test/core_make_p32.mk
@@ -26,7 +26,7 @@ LIB_LIST =                              \
         -lstdc++
 
 core_test:
-	powerpc-linux-gnu-g++ -O3 -g -DRT_CUSTOM=1 \
+	powerpc-linux-gnu-g++ -O3 -g \
         -DRT_LINUX -DRT_P32 -DRT_128=1 \
         -DRT_DEBUG=1 -DRT_PATH="../" \
         -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
