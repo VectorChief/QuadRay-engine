d7dad2f93ddc9d0808c111c0e3da32f70f5b111b
VectorChief <vectorchiefrocks@gmail.com>
2016-08-08 02:04:54 +0400

    add Win64 support to RooT demo via TDM64-GCC

---
+++ RooT_make_w64.mk
@@ -0,0 +1,51 @@
+
+INC_PATH =                              \
+        -Icore/config/                  \
+        -Icore/engine/                  \
+        -Icore/system/                  \
+        -Icore/tracer/                  \
+        -Idata/materials/               \
+        -Idata/objects/                 \
+        -Idata/scenes/                  \
+        -Idata/textures/
+
+SRC_LIST =                              \
+        core/engine/engine.cpp          \
+        core/engine/object.cpp          \
+        core/engine/rtgeom.cpp          \
+        core/engine/rtimag.cpp          \
+        core/system/system.cpp          \
+        core/tracer/tracer.cpp          \
+        core/tracer/tracer_128v1.cpp    \
+        core/tracer/tracer_128v2.cpp    \
+        core/tracer/tracer_128v4.cpp    \
+        core/tracer/tracer_256v1.cpp    \
+        core/tracer/tracer_256v2.cpp    \
+        RooT_win32.cpp
+
+LIB_PATH =
+
+LIB_LIST =                              \
+        -lm                             \
+        -lstdc++                        \
+        -lgdi32
+
+RooT:
+	g++ -O3 -g \
+        -DRT_WIN64 -DRT_X64 -DRT_128=1+2+4 -DRT_256=1+2 \
+        -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ENDIAN=0 \
+        -DRT_DEBUG=0 -DRT_PATH="./" -DRT_FULLSCREEN=0 \
+        -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
+        ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o RooT_w64f32.exe
+
+# Prerequisites for the build:
+# TDM64-GCC compiler for Win32/64 is installed and in the PATH variable.
+# Download tdm64-gcc-5.1.0-2.exe from sourceforge and run the installer.
+#
+# Use "MinGW Command Prompt" from "Windows Start Menu" under "TDM-GCC-64".
+# Works with regular "cmd" command prompt too after TDM64-GCC installation.
+#
+# Building/running CORE test:
+# run RooT_make_w64.bat file or
+# mingw32-make -f RooT_make_w64.mk
+# RooT_w64f32.exe
