71846577ba63c4a1f5ad2e607b9fd1ae60ebab1a
VectorChief <vectorchiefrocks@gmail.com>
2016-08-28 12:54:21 +0400

    adjust engine files to work with fp64 SIMD pipeline (part 3)

--- core/tracer/tracer_128v4.cpp
+++ core/tracer/tracer_128v4.cpp
@@ -23,13 +23,9 @@
 #define RT_RENDER_CODE /* enable contents of render0 routine */
 #endif /* RT_128 */
 
-#if   defined (RT_X86)
-#undef RT_RTARCH_X86_128_H
-#include "rtarch_x86_128.h"
-#elif defined (RT_X32) || defined (RT_X64)
-#undef RT_RTARCH_X32_128_H
-#include "rtarch_x32_128.h"
-#elif defined (RT_ARM)
+#if   RT_ELEMENT == 32
+
+#if   defined (RT_ARM)
 #undef RT_RTARCH_ARM_128_H
 #include "rtarch_arm_128.h"
 #elif defined (RT_A32) || defined (RT_A64)
@@ -41,7 +37,37 @@ exclude this file from compilation"
 #elif defined (RT_P32) || defined (RT_P64)
 #error "AltiVec doesn't have SIMD variant 4, \
 exclude this file from compilation"
-#endif /* RT_X86, RT_X32/X64, RT_ARM, RT_A32/A64, RT_M32/M64, RT_P32/P64 */
+#elif defined (RT_X32) || defined (RT_X64)
+#undef RT_RTARCH_X32_128_H
+#include "rtarch_x32_128.h"
+#elif defined (RT_X86)
+#undef RT_RTARCH_X86_128_H
+#include "rtarch_x86_128.h"
+#endif /* RT_ARM, RT_A32/A64, RT_M32/M64, RT_P32/P64, RT_X32/X64, RT_X86 */
+
+#elif RT_ELEMENT == 64
+
+#if   defined (RT_ARM)
+#error "UniSIMD doesn't support 64-bit SIMD elements in 32-bit mode, \
+adjust RT_ELEMENT build flag to be equal to 32"
+#elif defined (RT_A32) || defined (RT_A64)
+#error "AArch64 doesn't have SIMD variant 4, \
+exclude this file from compilation"
+#elif defined (RT_M32) || defined (RT_M64)
+#error "mipsMSA doesn't have SIMD variant 4, \
+exclude this file from compilation"
+#elif defined (RT_P32) || defined (RT_P64)
+#error "AltiVec doesn't have SIMD variant 4, \
+exclude this file from compilation"
+#elif defined (RT_X32) || defined (RT_X64)
+#undef RT_RTARCH_X64_128_H
+#include "rtarch_x64_128.h"
+#elif defined (RT_X86)
+#error "UniSIMD doesn't support 64-bit SIMD elements in 32-bit mode, \
+adjust RT_ELEMENT build flag to be equal to 32"
+#endif /* RT_ARM, RT_A32/A64, RT_M32/M64, RT_P32/P64, RT_X32/X64, RT_X86 */
+
+#endif /* RT_ELEMENT */
 
 /*
  * Global pointer tables
