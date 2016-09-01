a900ffcaf1ff712e35896359ae864ccd42d8b180
VectorChief <vectorchiefrocks@gmail.com>
2016-09-01 16:44:14 +0400

    rename PR64 printf mod to PR_Z (for signed 64-bit type)

--- test/core_test.cpp
+++ test/core_test.cpp
@@ -824,7 +824,7 @@ rt_pntr sys_alloc(rt_size size)
 
 #if RT_DEBUG >= 1
 
-    RT_LOGI("ALLOC PTR = %016"PR64"X, size = %ld\n", (rt_full)ptr, size);
+    RT_LOGI("ALLOC PTR = %016"PR_Z"X, size = %ld\n", (rt_full)ptr, size);
 
 #endif /* RT_DEBUG */
 
@@ -862,7 +862,7 @@ rt_void sys_free(rt_pntr ptr, rt_size size)
 
 #if RT_DEBUG >= 1
 
-    RT_LOGI("FREED PTR = %016"PR64"X, size = %ld\n", (rt_full)ptr, size);
+    RT_LOGI("FREED PTR = %016"PR_Z"X, size = %ld\n", (rt_full)ptr, size);
 
 #endif /* RT_DEBUG */
 }
@@ -919,7 +919,7 @@ rt_pntr sys_alloc(rt_size size)
 
 #if RT_DEBUG >= 1
 
-    RT_LOGI("ALLOC PTR = %016"PR64"X, size = %ld\n", (rt_full)ptr, size);
+    RT_LOGI("ALLOC PTR = %016"PR_Z"X, size = %ld\n", (rt_full)ptr, size);
 
 #endif /* RT_DEBUG */
 
@@ -957,7 +957,7 @@ rt_void sys_free(rt_pntr ptr, rt_size size)
 
 #if RT_DEBUG >= 1
 
-    RT_LOGI("FREED PTR = %016"PR64"X, size = %ld\n", (rt_full)ptr, size);
+    RT_LOGI("FREED PTR = %016"PR_Z"X, size = %ld\n", (rt_full)ptr, size);
 
 #endif /* RT_DEBUG */
 }
