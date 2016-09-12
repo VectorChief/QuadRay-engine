0b014bec6dfd45210f21132f2c9b334a47c2cfb9
VectorChief <vectorchiefrocks@gmail.com>
2016-09-12 21:32:39 +0300

    fix header files for 256-bit SIMD (AVX) in tracer

--- core/tracer/tracer_256v1.cpp
+++ core/tracer/tracer_256v1.cpp
@@ -38,11 +38,11 @@ exclude this file from compilation"
 #error "AltiVec doesn't support SIMD wider than 128-bit, \
 exclude this file from compilation"
 #elif defined (RT_X32) || defined (RT_X64)
-#undef RT_RTARCH_X32_128_H
-#include "rtarch_x32_128.h"
+#undef RT_RTARCH_X32_256_H
+#include "rtarch_x32_256.h"
 #elif defined (RT_X86)
-#undef RT_RTARCH_X86_128_H
-#include "rtarch_x86_128.h"
+#undef RT_RTARCH_X86_256_H
+#include "rtarch_x86_256.h"
 #endif /* RT_ARM, RT_A32/A64, RT_M32/M64, RT_P32/P64, RT_X32/X64, RT_X86 */
 
 #elif RT_ELEMENT == 64
@@ -60,8 +60,8 @@ exclude this file from compilation"
 #error "AltiVec doesn't support SIMD wider than 128-bit, \
 exclude this file from compilation"
 #elif defined (RT_X32) || defined (RT_X64)
-#undef RT_RTARCH_X64_128_H
-#include "rtarch_x64_128.h"
+#undef RT_RTARCH_X64_256_H
+#include "rtarch_x64_256.h"
 #elif defined (RT_X86)
 #error "UniSIMD doesn't support 64-bit SIMD elements in 32-bit mode, \
 adjust RT_ELEMENT build flag to be equal to 32"
