79d4e1671b3896ffd658a112af90a6c42400d6f0
VectorChief <vectorchiefrocks@gmail.com>
2016-11-04 01:14:34 +0200

    update test makefiles for 128-bit AVX(1,2) backend (on Windows)

--- RooT_make_w64.mk
+++ RooT_make_w64.mk
@@ -18,6 +18,7 @@ SRC_LIST =                              \
         core/tracer/tracer.cpp          \
         core/tracer/tracer_128v2.cpp    \
         core/tracer/tracer_128v4.cpp    \
+        core/tracer/tracer_128v8.cpp    \
         core/tracer/tracer_256v1.cpp    \
         core/tracer/tracer_256v2.cpp    \
         RooT_win32.cpp
@@ -41,7 +42,7 @@ clean:
 
 RooT_w64_32:
 	g++ -O3 -g -static -m64 \
-        -DRT_WIN64 -DRT_X64 -DRT_128=2+4 -DRT_256=1+2 \
+        -DRT_WIN64 -DRT_X64 -DRT_128=2+4+8 -DRT_256=1+2 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         -DRT_DEBUG=0 -DRT_PATH="./" -DRT_FULLSCREEN=0 \
         -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
@@ -49,7 +50,7 @@ RooT_w64_32:
 
 RooT_w64_64:
 	g++ -O3 -g -static -m64 \
-        -DRT_WIN64 -DRT_X64 -DRT_128=2+4 -DRT_256=1+2 \
+        -DRT_WIN64 -DRT_X64 -DRT_128=2+4+8 -DRT_256=1+2 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=64 -DRT_ENDIAN=0 \
         -DRT_DEBUG=0 -DRT_PATH="./" -DRT_FULLSCREEN=0 \
         -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
@@ -57,7 +58,7 @@ RooT_w64_64:
 
 RooT_w64f32:
 	g++ -O3 -g -static -m64 \
-        -DRT_WIN64 -DRT_X64 -DRT_128=2+4 -DRT_256=1+2 \
+        -DRT_WIN64 -DRT_X64 -DRT_128=2+4+8 -DRT_256=1+2 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         -DRT_DEBUG=0 -DRT_PATH="./" -DRT_FULLSCREEN=0 \
         -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
@@ -65,7 +66,7 @@ RooT_w64f32:
 
 RooT_w64f64:
 	g++ -O3 -g -static -m64 \
-        -DRT_WIN64 -DRT_X64 -DRT_128=2+4 -DRT_256=1+2 \
+        -DRT_WIN64 -DRT_X64 -DRT_128=2+4+8 -DRT_256=1+2 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=64 -DRT_ENDIAN=0 \
         -DRT_DEBUG=0 -DRT_PATH="./" -DRT_FULLSCREEN=0 \
         -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
