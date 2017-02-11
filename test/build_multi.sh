bbce1a3a0748cb0c6f2775a9dada943afbd28892
VectorChief <vectorchiefrocks@gmail.com>
2017-02-11 21:38:16 +0200

    accelerate release builds on multi-core machines

--- test/build_multi.sh
+++ test/build_multi.sh
@@ -3,8 +3,8 @@
 # with native g++ multilib-compiler installed (64-bit Linux Mint 17 tested)
 
 make -f simd_make_x86.mk build
-make -f simd_make_x32.mk build
-make -f simd_make_x64.mk build
+make -f simd_make_x32.mk build -j4
+make -f simd_make_x64.mk build -j4
 
 make -f simd_make_x86.mk strip
 make -f simd_make_x32.mk strip
@@ -12,8 +12,8 @@ make -f simd_make_x64.mk strip
 
 
 make -f core_make_x86.mk build
-make -f core_make_x32.mk build
-make -f core_make_x64.mk build
+make -f core_make_x32.mk build -j4
+make -f core_make_x64.mk build -j4
 
 make -f core_make_x86.mk strip
 make -f core_make_x32.mk strip
@@ -23,7 +23,7 @@ make -f core_make_x64.mk strip
 cd ../
 
 make -f RooT_make_x86.mk build
-make -f RooT_make_x64.mk build
+make -f RooT_make_x64.mk build -j4
 
 make -f RooT_make_x86.mk strip
 make -f RooT_make_x64.mk strip
