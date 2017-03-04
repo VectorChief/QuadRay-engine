361ee9e784db0dc93e0e7f6c30b42dc53e3899ab
VectorChief <vectorchiefrocks@gmail.com>
2017-03-04 21:29:01 +0200

    trigger target config switch for hide-screen-num mode, clean up

--- RooT.h
+++ RooT.h
@@ -39,27 +39,27 @@ rt_Scene   *sc[RT_ARR_SIZE(sc_rt)]  = {0};                  /* scene array */
 rt_si32     d                       = RT_ARR_SIZE(sc_rt)-1; /* demo-scene */
 rt_si32     c                       = 0;                    /* camera-idx */
 
-rt_si32     f_num       =-1; /* number-of-frames from command-line */
-rt_time     f_time      =-1; /* frame-delta (ms) from command-line */
-rt_time     b_time      = 0; /* time (ms) begins from command-line */
-rt_time     e_time      =-1; /* time (ms) ending from command-line */
-rt_si32     q_simd      = 0; /* SIMD quad-factor from command-line */
-rt_si32     s_type      = 0; /* SIMD sub-variant from command-line */
-rt_si32     t_pool      = 0; /* Thread-pool size from command-line */
+rt_si32     f_num       =-1; /* number-of-frames (from command-line) */
+rt_time     f_time      =-1; /* frame-delta (ms) (from command-line) */
+rt_time     b_time      = 0; /* time (ms) begins (from command-line) */
+rt_time     e_time      =-1; /* time (ms) ending (from command-line) */
+rt_si32     q_simd      = 0; /* SIMD quad-factor (from command-line) */
+rt_si32     s_type      = 0; /* SIMD sub-variant (from command-line) */
+rt_si32     t_pool      = 0; /* Thread-pool size (from command-line) */
 #if RT_FULLSCREEN == 1
-rt_si32     w_size      = 0; /* Window-rect size from command-line */
+rt_si32     w_size      = 0; /* Window-rect size (from command-line) */
 #else  /* RT_FULLSCREEN */
-rt_si32     w_size      = 1; /* Window-rect size from command-line */
+rt_si32     w_size      = 1; /* Window-rect size (from command-line) */
 #endif /* RT_FULLSCREEN */
-rt_si32     x_new       = 0; /* New x-resolution from command-line */
-rt_si32     y_new       = 0; /* New y-resolution from command-line */
+rt_si32     x_new       = 0; /* New x-resolution (from command-line) */
+rt_si32     y_new       = 0; /* New y-resolution (from command-line) */
 
-rt_time     l_time      = 500; /* fpslogupd (ms) from command-line */
-rt_bool     l_mode      = RT_FALSE; /* fpslogoff from command-line */
-rt_bool     h_mode      = RT_FALSE; /* hide-mode from command-line */
-rt_bool     o_mode      = RT_FALSE; /* offscreen from command-line */
-rt_bool     u_mode      = RT_FALSE; /* updateoff from command-line */
-rt_bool     a_mode      = RT_FALSE; /* FSAA-mode from command-line */
+rt_time     l_time      = 500; /* fpslogupd (ms) (from command-line) */
+rt_bool     l_mode      = RT_FALSE; /* fpslogoff (from command-line) */
+rt_bool     h_mode      = RT_FALSE; /* hide-mode (from command-line) */
+rt_bool     o_mode      = RT_FALSE; /* offscreen (from command-line) */
+rt_bool     u_mode      = RT_FALSE; /* updateoff (from command-line) */
+rt_bool     a_mode      = RT_FALSE; /* FSAA-mode (from command-line) */
 
 /******************************************************************************/
 /********************************   PLATFORM   ********************************/
@@ -152,12 +152,12 @@ rt_time run_time = 0;
 rt_bool switched = 0;
 
 /* frame counter variables */
-rt_si32 scr = 0;
-rt_real fps = 0.0f;
 rt_si32 cnt = 0;
-rt_real avg = 0.0f;
+rt_real fps = 0.0f;
 rt_si32 glb = 0;
+rt_real avg = 0.0f;
 rt_si32 ttl = 0;
+rt_si32 scr_id = 0;
 
 /* virtual key arrays */
 rt_byte r_to_p[KEY_MASK + 1];
@@ -300,7 +300,7 @@ rt_si32 main_step()
         }
         if (T_KEYS(RK_F4))
         {
-            sc[d]->save_frame(scr++);
+            sc[d]->save_frame(scr_id++);
             switched = 1;
         }
         if (T_KEYS(RK_F5))
@@ -331,6 +331,7 @@ rt_si32 main_step()
         if (T_KEYS(RK_F12))
         {
             h_mode = 1 - h_mode;
+            switched = 1;
         }
         if (T_KEYS(RK_ESCAPE))
         {
