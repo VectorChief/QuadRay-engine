b1f2fca4b81911ec199ea2bc451c28d09e271145
VectorChief <vectorchiefrocks@gmail.com>
2017-05-31 18:28:43 +0300

    add support for 30 SIMD registers (128/256) on Skylake X

--- root/RooT_make_x32.mk
+++ root/RooT_make_x32.mk
@@ -16,6 +16,7 @@ SRC_LIST =                                  \
         ../core/engine/rtimag.cpp           \
         ../core/system/system.cpp           \
         ../core/tracer/tracer.cpp           \
+        ../core/tracer/tracer_128v1.cpp     \
         ../core/tracer/tracer_128v2.cpp     \
         ../core/tracer/tracer_128v4.cpp     \
         ../core/tracer/tracer_128v8.cpp     \
@@ -23,6 +24,7 @@ SRC_LIST =                                  \
         ../core/tracer/tracer_256v4_r8.cpp  \
         ../core/tracer/tracer_256v1.cpp     \
         ../core/tracer/tracer_256v2.cpp     \
+        ../core/tracer/tracer_256v8.cpp     \
         ../core/tracer/tracer_512v1_r8.cpp  \
         ../core/tracer/tracer_512v2_r8.cpp  \
         ../core/tracer/tracer_512v1.cpp     \
@@ -55,7 +57,7 @@ clean:
 RooT_x32:
 	x86_64-linux-gnu-g++ -O3 -g -pthread -mx32 \
         -DRT_LINUX -DRT_X32 \
-        -DRT_128=2+4+8 -DRT_256_R8=2+4 -DRT_256=1+2 \
+        -DRT_128=1+2+4+8 -DRT_256_R8=2+4 -DRT_256=1+2+8 \
         -DRT_512_R8=1+2 -DRT_512=1+2 -DRT_1K4=1+2 -DRT_2K8_R8=0 \
         -DRT_POINTER=32 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         -DRT_DEBUG=0 -DRT_PATH="../" -DRT_FULLSCREEN=0 \
