7b8ca88ebd4431362ef41b639ba161c8bcb4ae0c
VectorChief <vectorchiefrocks@gmail.com>
2018-10-27 22:44:47 +0300

    clean up deprecated x86 from macOS build scripts

--- test/clean_macOS.sh
+++ test/clean_macOS.sh
@@ -2,11 +2,9 @@
 # Intended for x86_64 Mac OS X / OS X / macOS build environment
 # with Command Line Tools installed (Mac OS X Lion / macOS High Sierra tested)
 
-make -f simd_make_x86.mk macRM
 make -f simd_make_x64.mk macRM
 
 
-make -f core_make_x86.mk macRM
 make -f core_make_x64.mk macRM
 
 
