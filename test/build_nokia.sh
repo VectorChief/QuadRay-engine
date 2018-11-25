2dc6bb26e4fdeeb0fc7be0b97302e465c506d8f7
VectorChief <vectorchiefrocks@gmail.com>
2018-11-25 19:02:00 +0200

    clean up release build scripts

--- test/build_nokia.sh
+++ test/build_nokia.sh
@@ -2,12 +2,12 @@
 # Intended for scratchbox Linux build environment (32-bit Ubuntu 10.10 tested)
 # http://wiki.maemo.org/Documentation/Maemo_5_Final_SDK_Installation
 
-make -f simd_make_arm.mk build_n900 -j16
+make -f simd_make_arm.mk build_n900
 
 make -f simd_make_arm.mk strip_n900
 
 
-make -f core_make_arm.mk build_n900 -j16
+make -f core_make_arm.mk build_n900
 
 make -f core_make_arm.mk strip_n900
 
@@ -16,7 +16,7 @@ make -f core_make_arm.mk strip_n900
 
 cd ../root
 
-make -f RooT_make_arm.mk build_n900 -j16
+make -f RooT_make_arm.mk build_n900
 
 make -f RooT_make_arm.mk strip_n900
 
