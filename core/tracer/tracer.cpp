a5c2210287dd57cce17474468be67216c73457c4
VectorChief <vectorchiefrocks@gmail.com>
2019-11-13 19:15:28 +0400

    add PRNG splitter for ray depth in path-tracer

--- core/tracer/tracer.cpp
+++ core/tracer/tracer.cpp
@@ -2237,6 +2237,61 @@ rt_void render0(rt_SIMD_INFOX *s_inf)
         cmjxx_mz(Mebp, inf_PT_ON,
                  EQ_x, LT_reg)
 
+        cmjxx_mi(Mebp, inf_DEPTH, IB(RT_STACK_DEPTH - 5),
+                 GT_x, PT_cnt)
+
+        movpx_ld(Xmm1, Mecx, ctx_TEX_R)
+        movpx_ld(Xmm2, Mecx, ctx_TEX_G)
+        movpx_ld(Xmm3, Mecx, ctx_TEX_B)
+
+        movpx_rr(Xmm0, Xmm1)
+        cgtps_rr(Xmm0, Xmm2)
+        cgtps_rr(Xmm1, Xmm3)
+        andpx_rr(Xmm1, Xmm0)
+
+        CHECK_MASK(PT_clR, NONE, Xmm1)
+
+        movpx_ld(Xmm4, Mecx, ctx_TEX_R)
+        jmpxx_lb(PT_clB)
+
+    LBL(PT_clR)
+
+        cgtps_rr(Xmm2, Xmm3)
+
+        CHECK_MASK(PT_clG, NONE, Xmm2)
+
+        movpx_ld(Xmm4, Mecx, ctx_TEX_G)
+        jmpxx_lb(PT_clB)
+
+    LBL(PT_clG)
+
+        movpx_ld(Xmm4, Mecx, ctx_TEX_B)
+
+    LBL(PT_clB)
+
+        GET_RANDOM() /* -> Xmm0, destroys Xmm7, Reax */
+        movpx_st(Xmm0, Mecx, ctx_F_RND)
+        movpx_st(Xmm4, Mecx, ctx_F_PRB)
+
+        cltps_rr(Xmm0, Xmm4)
+
+        movpx_ld(Xmm1, Mecx, ctx_TEX_R)
+        movpx_ld(Xmm2, Mecx, ctx_TEX_G)
+        movpx_ld(Xmm3, Mecx, ctx_TEX_B)
+
+        rcpps_rr(Xmm5, Xmm4)
+        andpx_rr(Xmm5, Xmm0)
+
+        mulps_rr(Xmm1, Xmm5)
+        mulps_rr(Xmm2, Xmm5)
+        mulps_rr(Xmm3, Xmm5)
+
+        movpx_st(Xmm1, Mecx, ctx_TEX_R)
+        movpx_st(Xmm2, Mecx, ctx_TEX_G)
+        movpx_st(Xmm3, Mecx, ctx_TEX_B)
+
+    LBL(PT_cnt)
+
         xorpx_rr(Xmm7, Xmm7)                    /* tmp_v <-     0 */
 
         /* reset R */
