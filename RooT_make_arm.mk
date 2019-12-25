92ae8fcae0f88a32d7e4ea7d78f994c9a731e896
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 13:01:48 +0400

    update build options for cross-compilation

--- RooT_make_arm.mk
+++ RooT_make_arm.mk
@@ -27,5 +27,7 @@ LIB_LIST =                          \
 
 RooT:
 	g++ -O3 -g -march=armv7-a -marm \
-        -DRT_LINUX -DRT_ARM -DRT_DEBUG=0 -DRT_EMBED=1 -DRT_FULLSCREEN=1 \
+        -DRT_LINUX -DRT_ARM -DRT_DEBUG=0 -DRT_EMBED=0 -DRT_FULLSCREEN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o RooT.arm
+
+# For Maemo/N900 build enable: -DRT_EMBED=1 -DRT_FULLSCREEN=1.
