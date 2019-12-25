fccbdab4ead005664acd7b8a381bd3d47b7a7213
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 09:54:07 +0400

    update build options on Linux

--- test/simd_make_x86.mk
+++ test/simd_make_x86.mk
@@ -5,5 +5,5 @@ SRC_LIST =                          \
         simd_test.cpp
 
 simd_test:
-	g++ -O3 -g -m32 -DRT_X86        \
-        ${INC_PATH} ${SRC_LIST} -o simd_test.x86
+	g++ -O3 -g -static -m32 \
+        -DRT_X86 ${INC_PATH} ${SRC_LIST} -o simd_test.x86
