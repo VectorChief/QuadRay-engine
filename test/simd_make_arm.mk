8a674c60f996eb0f788ce918e6820b82cd535686
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 11:09:55 +0400

    update build options for cross-compilation

--- test/simd_make_arm.mk
+++ test/simd_make_arm.mk
@@ -5,5 +5,9 @@ SRC_LIST =                          \
         simd_test.cpp
 
 simd_test:
-	g++ -O3 -g -static -march=armv7-a -marm \
+	arm-linux-gnueabi-g++ -O3 -g -static -march=armv7-a -marm \
         -DRT_LINUX -DRT_ARM ${INC_PATH} ${SRC_LIST} -o simd_test.arm
+
+# Build target above is suitable for Maemo/N900 and DEB-based cross-compilation.
+# Other (than DEB-based) Linux systems may have different name for the compiler.
+# For native builds on ARMv7 (Raspberry Pi 2) use plain g++ reference.
