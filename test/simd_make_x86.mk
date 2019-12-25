894728e144f92f1f07030bfbee4dd1e0c6473390
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 11:47:38 +0400

    update build options on Linux (add -static for QEMU)

--- test/simd_make_x86.mk
+++ test/simd_make_x86.mk
@@ -5,5 +5,6 @@ SRC_LIST =                          \
         simd_test.cpp
 
 simd_test:
-	g++ -O3 -g -m32 -DRT_X86 -DRT_DEBUG=0 \
+	g++ -O3 -g -static -m32 \
+        -DRT_X86 -DRT_DEBUG=0 \
         ${INC_PATH} ${SRC_LIST} -o simd_test.x86
