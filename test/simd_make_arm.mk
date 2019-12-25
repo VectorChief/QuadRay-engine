607703ecc5e5aa023468b8d29e2f6a049c6c1ea2
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 10:49:03 +0400

    update build options on Linux

--- test/simd_make_arm.mk
+++ test/simd_make_arm.mk
@@ -5,5 +5,5 @@ SRC_LIST =                          \
         simd_test.cpp
 
 simd_test:
-	g++ -O3 -g -DRT_ARM             \
-        ${INC_PATH} ${SRC_LIST} -o simd_test.arm
+	g++ -O3 -g -static -march=armv7-a -marm \
+        -DRT_ARM ${INC_PATH} ${SRC_LIST} -o simd_test.arm
