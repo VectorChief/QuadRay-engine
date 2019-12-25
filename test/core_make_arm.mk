d12b61dbf8a6eb330a56b5f5c40123dcd5d1345e
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 14:48:19 +0400

    update build options for cross-compilation

--- test/core_make_arm.mk
+++ test/core_make_arm.mk
@@ -19,8 +19,13 @@ SRC_LIST =                          \
         core_test.cpp
 
 core_test:
-	g++ -O3 -g -static -march=armv7-a -marm \
+	arm-linux-gnueabi-g++ -O3 -g -static -march=armv7-a -marm \
         -DRT_PATH="../" \
         -DRT_LINUX -DRT_ARM -DRT_DEBUG=1 \
-        -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=1 -DRT_EMBED_TEX=1 \
+        -DRT_EMBED_STDOUT=0 -DRT_EMBED_FILEIO=0 -DRT_EMBED_TEX=1 \
         ${INC_PATH} ${SRC_LIST} -o core_test.arm
+
+# Build target above is suitable for Maemo/N900 and DEB-based cross-compilation.
+# Other (than DEB-based) Linux systems may have different name for the compiler.
+# For native builds on ARMv7 (Raspberry Pi 2) use plain g++ reference.
+# For Maemo/N900 build enable: -DRT_EMBED_FILEIO=1.
