42e14c1032319da2e33538145c075847aec32246
VectorChief <vectorchiefrocks@gmail.com>
2020-06-24 19:30:12 +0300

    update standalone MIPS compiler to 2020.06-01

--- test/core_make_m64.mk
+++ test/core_make_m64.mk
@@ -31,7 +31,8 @@ LIB_LIST =                                  \
 build: build_le build_be
 
 strip:
-	mips-img-linux-gnu-strip core_test.m64*
+	mips-mti-linux-gnu-strip core_test.m64???Lr6
+	mips-mti-linux-gnu-strip core_test.m64???Br6
 
 clean:
 	rm core_test.m64*
@@ -41,7 +42,7 @@ build_le: core_test_m64_32Lr6 core_test_m64_64Lr6 \
           core_test_m64f32Lr6 core_test_m64f64Lr6
 
 core_test_m64_32Lr6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_256=1 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         -DRT_DEBUG=0 -DRT_PATH="../" \
@@ -49,7 +50,7 @@ core_test_m64_32Lr6:
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o core_test.m64_32Lr6
 
 core_test_m64_64Lr6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_256=1 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=64 -DRT_ENDIAN=0 \
         -DRT_DEBUG=0 -DRT_PATH="../" \
@@ -57,7 +58,7 @@ core_test_m64_64Lr6:
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o core_test.m64_64Lr6
 
 core_test_m64f32Lr6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_256=1 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         -DRT_DEBUG=0 -DRT_PATH="../" \
@@ -65,7 +66,7 @@ core_test_m64f32Lr6:
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o core_test.m64f32Lr6
 
 core_test_m64f64Lr6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_256=1 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=64 -DRT_ENDIAN=0 \
         -DRT_DEBUG=0 -DRT_PATH="../" \
@@ -77,7 +78,7 @@ build_be: core_test_m64_32Br6 core_test_m64_64Br6 \
           core_test_m64f32Br6 core_test_m64f64Br6
 
 core_test_m64_32Br6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_256=1 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=1 \
         -DRT_DEBUG=0 -DRT_PATH="../" \
@@ -85,7 +86,7 @@ core_test_m64_32Br6:
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o core_test.m64_32Br6
 
 core_test_m64_64Br6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_256=1 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=64 -DRT_ENDIAN=1 \
         -DRT_DEBUG=0 -DRT_PATH="../" \
@@ -93,7 +94,7 @@ core_test_m64_64Br6:
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o core_test.m64_64Br6
 
 core_test_m64f32Br6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_256=1 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=32 -DRT_ENDIAN=1 \
         -DRT_DEBUG=0 -DRT_PATH="../" \
@@ -101,7 +102,7 @@ core_test_m64f32Br6:
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o core_test.m64f32Br6
 
 core_test_m64f64Br6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_256=1 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=64 -DRT_ENDIAN=1 \
         -DRT_DEBUG=0 -DRT_PATH="../" \
@@ -111,32 +112,24 @@ core_test_m64f64Br6:
 
 # On Ubuntu (MATE) 16.04/18.04 download MIPS toolchain:
 # https://www.mips.com/develop/tools/codescape-mips-sdk/
-# https://codescape.mips.com/components/toolchain/2017.10-08/downloads.html
+# https://codescape.mips.com/components/toolchain/2020.06-01/downloads.html
 #
 # Prerequisites for the build:
 # (cross-)compiler for MIPSr6+MSA is installed and in the PATH variable.
-# Codescape.GNU.Tools.Package.2017.10-08.for.MIPS.IMG.Linux.CentOS-5.x86_64
-# is unpacked and folder mips-img-linux-gnu/2017.10-08/bin is added to PATH:
-# PATH=/home/ubuntu/Downloads/mips-img-linux-gnu/2017.10-08/bin:$PATH
-# PATH=/home/ubuntu-mate/Downloads/mips-img-linux-gnu/2017.10-08/bin:$PATH
-#
-# Starting from 2019.09-01 toolchains MTI (r5) and IMG (r6) merged into one.
-# https://codescape.mips.com/components/toolchain/2019.09-01/downloads.html
-# Codescape.GNU.Tools.Package.2019.09-01.for.MIPS.MTI.Linux.CentOS-6.x86_64
-# is unpacked and folder mips-mti-linux-gnu/2019.09-01/bin is added to PATH:
-# PATH=/home/ubuntu/Downloads/mips-mti-linux-gnu/2019.09-01/bin:$PATH
-# PATH=/home/ubuntu-mate/Downloads/mips-mti-linux-gnu/2019.09-01/bin:$PATH
+# Codescape.GNU.Tools.Package.2020.06-01.for.MIPS.MTI.Linux.CentOS-6.x86_64
+# is unpacked and folder mips-mti-linux-gnu/2020.06-01/bin is added to PATH:
+# PATH=/home/ubuntu/Downloads/mips-mti-linux-gnu/2020.06-01/bin:$PATH
+# PATH=/home/ubuntu-mate/Downloads/mips-mti-linux-gnu/2020.06-01/bin:$PATH
 #
 # Starting from Ubuntu (MATE) 19.10 upstream (cross-)compiler supports MSA.
 # sudo apt-get install make g++-mipsisa64r6el-linux-gnuabi64
 # sudo apt-get install make g++-mipsisa64r6-linux-gnuabi64
-# (replace mips-img-linux-gnu-g++ with mipsisa64r6el-linux-gnuabi64-g++ for LE)
-# (replace mips-img-linux-gnu-g++ with mipsisa64r6-linux-gnuabi64-g++ for BE)
-# (replace -img- with -mti- to use single standalone toolchain from 2019.09-01)
+# (replace mips-mti-linux-gnu-g++ with mipsisa64r6el-linux-gnuabi64-g++ for LE)
+# (replace mips-mti-linux-gnu-g++ with mipsisa64r6-linux-gnuabi64-g++ for BE)
 #
 # Prerequisites for emulation:
 # recent QEMU(-2.7) is installed or built from source and in the PATH variable.
-# standalone toolchain from 2019.09-01 comes with QEMU 4.1.0 for MIPS in PATH.
+# standalone toolchain from 2020.06-01 comes with QEMU 4.1.0 for MIPS in PATH.
 # sudo apt-get install qemu-user make
 #
 # Compiling/running CORE test:
