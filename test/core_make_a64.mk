78bb2427664ea4412a2b76304f12a72b3a1d3a96
VectorChief <vectorchiefrocks@gmail.com>
2023-04-05 17:56:56 +0300

    update makefile comments for Ubuntu 22.04

--- test/core_make_a64.mk
+++ test/core_make_a64.mk
@@ -115,7 +115,7 @@ core_test_a64f64sve:
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o core_test.a64f64sve
 
 
-# On Ubuntu (MATE) 16.04-20.04 add "universe multiverse" to "main restricted"
+# On Ubuntu (MATE) 16.04-22.04 add "universe multiverse" to "main restricted"
 # in /etc/apt/sources.list (sudo nano /etc/apt/sources.list) then run:
 # sudo apt-get update
 # (Ubuntu MATE is set up for an update without a need to edit the file)
